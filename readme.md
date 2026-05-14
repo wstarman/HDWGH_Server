916e16e42a831c408d2f421a834ce4ca31fb9c1e
wstarman <wstarese@gmail.com>
2026-05-15 00:38:35 +0800

    init

---
+++ readme.md
@@ -0,0 +1,4 @@
+# 建置流程：
+
+1. docker compose up --build
+2. 聯繫翁世承如果這不管用
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("hello");
+});
+
+app.listen(3000, () => {
+  console.log("Hello World!");
+});
