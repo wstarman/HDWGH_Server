9216a7ff82a927eeb47580f999bf8c46e5cbef33
KillerType59 <144765417+KillerType59@users.noreply.github.com>
2026-05-23 07:06:21 +0800

    life when you can use api but remove p

---
+++ prisma/migrations/20260522184607_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Character" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "Character_pkey" PRIMARY KEY ("id")
+);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client';
+
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.get("/", (_, res) => {
   res.send("hello");
+});
+
+app.get("/Character/:id", async (req, res) => {
+  const {id} = req.params;
+
+  console.log(id);
+
+  try{
+    const character = await prisma.character.findUnique({
+      where: { id: parseInt(id) }
+    });
+
+    if(character) res.json(character);
+    else res.status(404).json({ error: 'Character not found' });
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 app.listen(3000, () => {
